a8eb1442c0b569c4344b1eed405311a68e222f43
jwvdriel <janwillemvandriel@gmail.com>
2025-06-13 22:09:05 +0200

    update config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/space-card/',
+  base: 'https://jwvdriel.github.io/space-card/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
